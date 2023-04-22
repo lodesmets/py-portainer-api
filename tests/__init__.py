9afd4ddc30963682398badefa2d0a7ce7b77f019
lodesmets <smets.lode97@gmail.com>
2023-04-22 09:05:46 +0000

    tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,22 +1,38 @@
 """Library tests."""
-import re
-
-from requests import Response
+from portainer import Portainer
 
 
-class PortainerMock:
+class PortainerMock(Portainer):
     """Mocked Portainer."""
 
-    async def run_command(
-        self, method: str, api: str, params: dict | None, auto_login: bool = True
-    ) -> Response:
-        """Mocked run_command."""
-        del method, params, auto_login
-        arguments = re.findall(r"{(.*?)}", api)
-        if "environment_id" in arguments:
-            api = api.format(environment_id=1)
-        if "container_id" in arguments:
-            api = api.format(container_id=1)
-        ret = Response()
-        ret.status_code = 200
+    def __init__(
+        self,
+        session,
+        portainer_ip,
+        portainer_port,
+        username,
+        password,
+        timeout: int = 600,
+        use_https: bool = False,
+        debugmode: bool = False,
+    ):
+        """Constructor method."""
+        Portainer.__init__(
+            self,
+            session,
+            portainer_ip,
+            portainer_port,
+            username,
+            password,
+            timeout,
+            use_https,
+            debugmode,
+        )
+
+    async def _execute_request(
+        self, method: str, url: str, params: dict | None, headers: dict | None = None
+    ) -> dict:
+        ret: dict[str, str | int] = {}
+        ret["status_code"] = 200
+        ret["body"] = "test"
         return ret
--- tests/confest.py
+++ tests/confest.py
@@ -3,8 +3,10 @@ import pytest
 
 from portainer import Portainer
 
+from . import PortainerMock
+
 
 @pytest.fixture
 def api() -> Portainer:
     """Return a mock portainer API."""
-    return Portainer("192.168.0.1", 9000, "admin", "password")
+    return PortainerMock(None, "192.168.0.1", 9000, "admin", "password")
